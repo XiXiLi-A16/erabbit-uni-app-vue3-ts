5034b43fc4617df580804caca23b68f3f71040f4
Lin_麓 <437552389@qq.com>
2025-02-09 16:57:19 +0800

    feat 添加“猜你喜欢”组件并更新首页布局以支持新组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -11,7 +11,7 @@ const bannerList = ref([])
 const getHomeBannerData = async () => {
   const res = await getHomeBannerApi()
   bannerList.value = res.result
-  console.log(bannerList.value)
+  // console.log(bannerList.value)
 }
 
 // 获取前台分类数据
@@ -36,14 +36,28 @@ onLoad(() => {
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <XtxSwiper :list="bannerList" />
-  <CategoryPanel :list="categoryList" />
-  <HotPanel :list="hotList" />
+  <scroll-view class="scroll-view" scroll-y>
+    <!-- 自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 分类面板 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐 -->
+    <HotPanel :list="hotList" />
+    <!-- 猜你喜欢 -->
+    <XtxGuess />
+  </scroll-view>
 </template>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,11 +1,9 @@
-/**
- * declare module '@vue/runtime-core'
- *   现调整为
- * declare module 'vue'
- */
-import 'vue'
+// 全局组件的类型声明
+import XtxSwiper from './XtxSwiper.vue'
+import XtxGuess from './XtxGuess.vue'
 declare module 'vue' {
   export interface GlobalComponents {
-    //
+    XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
